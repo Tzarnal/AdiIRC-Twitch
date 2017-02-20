cea9cce5399aef75d8e3952abafe26b7eee61744
Xesyto <s.oudenaarden@gmail.com>
2017-02-20 16:05:15 +0100

    Added handling of NOTICE, CLEARCHAT, ROOMSTATE, USERSTATE and USERNOTICE messages.

--- Twitch @ AdiIRC/Twitch @ AdiIRC/TwitchAdiIRC.cs
+++ Twitch @ AdiIRC/Twitch @ AdiIRC/TwitchAdiIRC.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using AdiIRCAPI;
 
 namespace TwitchAdiIRC
@@ -18,7 +19,7 @@ namespace TwitchAdiIRC
         public IPluginHost Host { get; set; }
         public ITools Tools { get; set; }
         
-        private string _emoteDirectory =  Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\AdiIRC\TwitchEmotes";        
+        private readonly string _emoteDirectory =  Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\AdiIRC\TwitchEmotes";        
         private List<string> _handledEmotes;
         private IServer _twitchServer;
 
@@ -74,7 +75,65 @@ namespace TwitchAdiIRC
                 {                    
                     RegisterEmotes(twitchMessage.Emotes);
                 }
-            }                       
+
+                return;
+            }
+
+            //Pricess Message, redirect notices to proper channel tab
+            if (dataString.Contains("NOTICE "))
+            {
+                //Check if its a usable notiec message
+                var noticeRegex = @".+ :tmi.twitch.tv NOTICE (#.+) :(.+)";
+                var noticeMatch = Regex.Match(dataString, noticeRegex);
+
+                if (noticeMatch.Success)
+                {
+                    var channel = noticeMatch.Groups[1].ToString();
+                    var message = noticeMatch.Groups[2].ToString();
+
+                    //Send a fake regular irc notice instead
+                    
+                    var notice = $":Twitch!Twitch@Twitch.tv NOTICE {channel} :{message}";
+                    _twitchServer.SendFakeRaw(notice);
+
+                    //Eat message.
+                    rawDataArgs.Bytes = null;
+                    return;
+                }
+            }
+
+            if (dataString.Contains("CLEARCHAT ") )
+            {
+                var clearChatRegex = @"@ban-duration=(.*?);ban-reason=(.*?);.+ :tmi.twitch.tv CLEARCHAT (#.+) :(.+)";
+                var clearChatMatch = Regex.Match(dataString, clearChatRegex);
+
+                if (clearChatMatch.Success)
+                {
+                    var channel = clearChatMatch.Groups[3].ToString();
+                    var message = clearChatMatch.Groups[2].ToString().Replace("\\s"," ");
+                    var time = clearChatMatch.Groups[1].ToString();
+                    var target = clearChatMatch.Groups[4].ToString();
+
+                    if (string.IsNullOrEmpty(time))
+                        time = "∞";
+
+                    if (string.IsNullOrEmpty(message))
+                        message = "No Reason Given";
+
+                    var notice = $":Twitch!Twitch@Twitch.tv NOTICE {channel} :{target} was banned: {message} [{time} seconds]";
+                    _twitchServer.SendFakeRaw(notice);
+
+                    //Eat message.
+                    rawDataArgs.Bytes = null;
+                    return;
+                }                
+            }
+
+            if (dataString.Contains("ROOMSTATE ") || dataString.Contains("USERSTATE ") || dataString.Contains("USERNOTICE "))
+            {
+                //Silently eat these messages and do nothing. They only cause empty * lines to appear in the server tab and Twitch@AdiIRC does not use them
+                rawDataArgs.Bytes = null;
+            }                                    
         }
 
         public void RegisterEmote(TwitchEmote emote)
@@ -86,8 +145,10 @@ namespace TwitchAdiIRC
 
             if (File.Exists(emoteFile))
             {
+                
                 //Actually register the emote with AdiIRC
-                Host.SendCommand(_twitchServer, ".setoption", $"Emoticons Emoticon_{emote.Name} {emoteFile}");
+                var command = $"Emoticons Emoticon_{emote.Name} {emoteFile}";
+                Host.SendCommand(_twitchServer, ".setoption", command);
 
                 _handledEmotes.Add(emote.Name);
                 return;
@@ -96,7 +157,8 @@ namespace TwitchAdiIRC
             if (DownloadEmote(emote))
             {
                 //Actually register the emote with AdiIRC
-                Host.SendCommand(_twitchServer, ".setoption", $"Emoticons Emoticon_{emote.Name} {emoteFile}");
+                var command = $"Emoticons Emoticon_{emote.Name} {emoteFile}";
+                Host.SendCommand(_twitchServer, ".setoption", command);                
 
                 _handledEmotes.Add(emote.Name);
             }
@@ -119,7 +181,7 @@ namespace TwitchAdiIRC
                 WebClient wc = new WebClient();
                 wc.DownloadFile(emote.URL, filePath);
             }
-            catch (Exception e)
+            catch (Exception)
             {                
                 return false;
             }
